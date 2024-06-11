273fc5daa32160998c77f0c1cb6b6d0365ebbfed
Yan Zhang <yanyzhang1@gmail.com>
2024-06-10 22:32:27 -0400

    deleted data.js

--- src/mainpart2.jsx
+++ src/mainpart2.jsx
@@ -1,15 +1,14 @@
 import React from "react";
-import data from "./data";
 
-// const data = [
-//   { month: "June", day: "17", time: "10:00 AM", staff: "Kangaroo" },
-//   { month: "June", day: "17", time: "11:00 AM", staff: "Kangaroo" },
-//   { month: "June", day: "17", time: "1:00 AM", staff: "Kangaroo" },
-//   { month: "June", day: "17", time: "9:00 AM", staff: "Owl" },
-//   { month: "June", day: "17", time: "9:30 AM", staff: "Donkey" },
-//   { month: "June", day: "18", time: "10:00 AM", staff: "Donkey" },
-//   { month: "June", day: "20", time: "4:00 pm", staff: "Owl" },
-// ];
+const data = [
+  { month: "June", day: "17", time: "10:00 AM", staff: "Kangaroo" },
+  { month: "June", day: "17", time: "11:00 AM", staff: "Kangaroo" },
+  { month: "June", day: "17", time: "1:00 AM", staff: "Kangaroo" },
+  { month: "June", day: "17", time: "9:00 AM", staff: "Owl" },
+  { month: "June", day: "17", time: "9:30 AM", staff: "Donkey" },
+  { month: "June", day: "18", time: "10:00 AM", staff: "Donkey" },
+  { month: "June", day: "20", time: "4:00 pm", staff: "Owl" },
+];
 
 function Mainpart2() {
   return (
